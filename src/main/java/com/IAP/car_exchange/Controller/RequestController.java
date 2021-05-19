17fdac841857b9c31556179c47906bbeb98731b3
muga01 <gmusomali@gmail.com>
2021-05-20 00:32:15 +0200

    no changes, huhu

--- src/main/java/com/IAP/car_exchange/Controller/RequestController.java
+++ src/main/java/com/IAP/car_exchange/Controller/RequestController.java
@@ -4,13 +4,16 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.IAP.car_exchange.Controller.DataHolders.RequestData;
+import com.IAP.car_exchange.Model.Office;
 import com.IAP.car_exchange.Model.Request;
 import com.IAP.car_exchange.repository.Querries;
 
@@ -39,6 +42,18 @@ public class RequestController {
 	public @ResponseBody Iterable<Request> getRequests(){
 		return DataAccess.getAllRequests();
 	}
+	
+    @GetMapping("request/{id}")
+    public Request getRequest(@PathVariable Long id){
+        return DataAccess.getRequestById(id);
+    }
+    
+    @DeleteMapping("_request/{id}")
+    public ResponseEntity<String> deleteRequest(@PathVariable("id") Long id){
+        DataAccess.deleteRequest(id);
+        return ResponseEntity.ok("Removed");
+    }
+
 	
 
 }
