efab60bc19afb27e18edfa1e2c8d40115683865b
Sosnus <sosnus15@gmail.com>
2021-05-25 15:28:30 +0200

    add chafix create db

--- initmysql8-v3.sql
+++ initmysql8-v3.sql
@@ -19,8 +19,8 @@
 -- Table structure for table `cars`
 --
 
-CREATE DATABASE iap-test;
-USE iap-test;
+CREATE DATABASE `iap-test`;
+USE `iap-test`;
 
 DROP TABLE IF EXISTS `cars`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
